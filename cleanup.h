a6def046d1816a78b7b8ef62fd7183dd1a8d408a
ForwardsHao <30368596+xuhao531799712@users.noreply.github.com>
2021-01-07 22:26:40 +0800

    Update cleanup.h

--- cleanup.h
+++ cleanup.h
@@ -2,5 +2,7 @@
 #define BERT_CLEANUP_H
 
 #define on_destroy(T, _cleanup)  __attribute__((cleanup(_cleanup))) T
+// __attribute__((cleanup(...)))，用于修饰一个变量，在它的作用域结束时可以自动执行一个指定的方法。
+// 而且，cleanup是先于这个对象的dealloc调用的。
 
 #endif
